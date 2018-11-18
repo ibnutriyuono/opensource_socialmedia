2b81a7ebc8dd9680af7cfe91fcb67ddd3f563b74
ibnutriyuono <ibnutriyuono@gmail.com>
2018-11-18 19:33:12 +0700

    secret ket ssst..

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://ibnu:lastagg23@ds211694.mlab.com:11694/opensource_socialmedia'
+    mongoURI: 'mongodb://ibnu:lastagg23@ds211694.mlab.com:11694/opensource_socialmedia',
+    secretOrKey: 'secret'
 }
