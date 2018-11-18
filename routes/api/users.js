e8d0e2f85624f9a46bac073286a30c8e20f1873c
ibnutriyuono <ibnutriyuono@gmail.com>
2018-11-18 19:33:36 +0700

    add jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// keys
+const keys = require('../../config/keys');
 
 // load user model
 const User = require('../../models/User');
@@ -82,9 +86,25 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({
-                            msg: "Success"
-                        })
+                        // User matched
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        }
+
+                        // sign token
+                        jwt.sign(payload,
+                            keys.secretOrKey, {
+                                expiresIn: 3600
+                            },
+                            (err, token) => {
+                                res.json({
+                                    success:true,
+                                    token: 'Bearer '+ token
+                                });
+                            });
+
                     } else {
                         return res.status(400).json({
                             password: "password incorrect"
