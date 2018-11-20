4c420c709972e0a390fb6c4a871a2f13bee61a16
ibnutriyuono <ibnutriyuono@gmail.com>
2018-11-20 17:41:59 +0700

    adding get profile for current user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.get('/test', (req, res)=>{
+// load model
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+/*  @route GET /api/profile/test
+    @desc test profile route
+    @access public
+*/
+router.get('/test', (req, res) => {
     res.json({
         msg: "Profile works!"
     });
 });
+
+/*  @route GET /api/profile
+    @desc GET current profile 
+    @access private
+*/
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const errors = {};
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile =>{
+            if(!profile){
+                errors.noProfile = 'There is no profile for this user'
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err=>{
+            res.status(404).json(err);
+        })
+})
 
 module.exports = router;
