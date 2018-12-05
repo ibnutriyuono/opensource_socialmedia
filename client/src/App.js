0947b4491c965ff135c37e76418bb2ca4c7f5cdb
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-05 11:15:55 +0700

    adding landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
-        <h1>MItythegoat</h1>
+        <Landing/>
         <Footer/>
       </div>
     );
