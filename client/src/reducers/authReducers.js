c10284434967aa1d330dcc59ad87ea938f2c6dff
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-15 17:16:14 +0700

    adding set current user case

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,3 +1,6 @@
+import {SET_CURRENT_USER} from '../actions/types';
+import isEmpty from '../validation/is_empty';
+
 const initialState = {
     isAuthenticated: false,
     user: {},
@@ -6,11 +9,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch(action.type){
-        // case TEST_DISPATCH:
-        //     return {
-        //         ...state,
-        //         user: action.payload
-        //     }
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            }
         default:
             return state;
     }
