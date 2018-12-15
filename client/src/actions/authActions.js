2fb7c434ac78da413063759e3c74e8f6699e635f
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-15 17:15:53 +0700

    adding set current user for login

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import {GET_ERRORS} from './types';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // register
 export const registerUser = (userData, history) => dispatch => {
@@ -22,7 +24,13 @@ export const registerUser = (userData, history) => dispatch => {
 export const loginUser = userData => dispatch => {
     axios.post('/api/users/login', userData)
         .then(res=> {
-
+            const {token} = res.data;
+            localStorage.setItem('jwtToken', token);
+            // set token to auth header
+            setAuthToken(token);
+            // decode token to get data
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded));
         })
         .catch(e => 
             dispatch({
@@ -30,4 +38,11 @@ export const loginUser = userData => dispatch => {
                 payload: e.response.data
             })
         )
+}
+
+export const setCurrentUser = (decoded) => {
+    return{
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
 }
