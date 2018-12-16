bb8c77c6a4a933c43b957507fcb6045d2fb0c1f2
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-16 19:05:56 +0700

    adding redux, proptypes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import moduleName from '../../actions/authActions';
 
 class Navbar extends Component {
   render() {
