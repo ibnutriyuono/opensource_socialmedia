65917b5da5594fa9c16cb37c2d80cf6bee53fed8
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-27 08:39:38 +0700

    fix PropTypes is not defined

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const TextFieldGroup = ({
     name,
@@ -34,19 +34,20 @@ const TextFieldGroup = ({
   )
 }
 
-TextFieldGroup.propTypes={
-    name= PropTypes.string.isRequired,
-    placeholder= PropTypes.string,
-    value= PropTypes.string.isRequired,
-    info= PropTypes.string,
-    error= PropTypes.string,
-    type= PropTypes.string.isRequired,
-    onChange= PropTypes.func.isRequired,
-    disabled= PropTypes.string
-}
-
-TextFieldGroup.defaultProps = {
+TextFieldGroup.propTypes = {
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    info: PropTypes.string,
+    error: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.string
+  };
+  
+  TextFieldGroup.defaultProps = {
     type: 'text'
-}
-
-export default TextFieldGroup;
+  };
+  
+  export default TextFieldGroup;
+  
