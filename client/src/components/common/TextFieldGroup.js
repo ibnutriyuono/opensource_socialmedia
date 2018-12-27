188487f12e7bc223673072f9cb9aa4597f9d1202
ibnutriyuono <ibnutriyuono@gmail.com>
2018-12-27 08:21:56 +0700

    adding some props and import some dependencies

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from 'react';
+import classnames from 'classnames';
+import propTypes from 'prop-types';
 
-const TextFieldGroup = () => {
+const TextFieldGroup = ({
+    name,
+    placeholder,
+    value,
+    label,
+    error,
+    info,
+    type,
+    onChange,
+    disabled
+}) => {
   return (
     <div>
       
